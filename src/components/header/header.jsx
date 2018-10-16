9e9c710b1ebb07a9860f0baef825db8a7625655e
Amber Kim <ambergkim@gmail.com>
2018-10-15 21:10:27 -0700

    trying to fix image loading

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,12 +4,14 @@ import FontAwesome from 'react-fontawesome';
 
 // import './header.scss';
 
+import logo from '../../images/nouri-logo.png'
+
 class Header extends React.Component {
 
   render() {
     return <header>
         <nav class="header-nav">
-          <img src="./src/images/nouri-logo.png" alt="Nouri Logo" id="logo"/>
+          <img src={logo} alt="Nouri Logo" id="logo"/>
           <div class="nav-links">
             <a href="mailto:ambergkim@gmail.com">
               <FontAwesome name="envelope" size="2x" />
