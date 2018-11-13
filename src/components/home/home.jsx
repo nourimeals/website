a21a66df5f7e48be01b0a0c93b2da63cad07438c
ryanscottjohnson <teacherman1@gmail.com>
2018-11-13 14:54:38 -0800

    cta-block width adjusted, height still covering hero image

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -30,21 +30,24 @@ class HomePage extends React.Component {
 
   render() {
     return <React.Fragment>
+
         <section id="hero-block">
           <div className="cta-block">
             <h1>Will you be a part of
             your community?</h1>
-            <p>Fighting childhood hunger starts with schools…</p>
-            <p>… and ends with a vibrant and supportive community.</p>
+            <p>Fighting childhood hunger starts with schools…<br/>
+            … and ends with a vibrant and supportive community.</p>
             <button onClick={this.runMailPopup} className="cta-btn">Connect</button>
           </div>
         </section>
+
         <section id="home-quotes">
           <div className="white-overlay quote-content">
             <blockquote cite="https://www.nokidhungry.org/who-we-are/hunger-facts">More than 13 million children in the US live in ‘food insecure’ homes… In the US, 1 in 6 children face hunger.<cite> - nokidhungry.org</cite></blockquote>
             <blockquote cite="https://www.northwestharvest.org/wa-hunger-facts">1 in 5 kids in Washington state lives in a household that struggles to put food on the table. The majority of working-age Washingtonians who live in poverty are actively working or looking for work.<cite> - northwestharvest.org</cite></blockquote>
           </div>
         </section>
+
         <section id="home-come-in">
           <p>Time is one thing that holds people back from improving their situation and time is also what often holds other back from being more involved and invested in the wellbeing of their communities.</p>
           <h1>This is where Nouri Meals comes in.</h1>
@@ -54,6 +57,7 @@ class HomePage extends React.Component {
           <h1>Learn more...</h1>
           <button onClick={this.runMailPopup} className="cta-btn">Connect</button>
         </section>
+
         <section id="home-how-it-works">
           <h1>How does Nouri Meals work?</h1>
           <ul>
@@ -63,6 +67,7 @@ class HomePage extends React.Component {
             <li>Vendors deliver ready-to-eat food to families</li>
           </ul>
         </section>
+
       </React.Fragment>
   }
 }
