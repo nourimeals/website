3dfedda07439714527c82b6c0ae87e6a8a9eed3b
Amber Kim <ambergkim@gmail.com>
2019-02-03 10:27:45 -0800

    quick fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'build'));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
