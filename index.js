29c42c4d8ed1c4cd4cde7880bed69bc169ef1bc6
Amber Kim <ambergkim@gmail.com>
2019-02-03 10:25:18 -0800

    trial

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(path.join(__dirname, 'build'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/build/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, (req, res) => {
