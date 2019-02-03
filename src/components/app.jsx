7bc0bb98de81c282e941182981859803ca34a084
Amber Kim <ambergkim@gmail.com>
2019-02-02 23:19:45 -0800

    ran build

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
@@ -7,7 +9,7 @@ import About from './about/about.jsx';
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/about' component={About} />
