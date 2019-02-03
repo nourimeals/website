abe19d7cad7c9158386e02ceab0ee8e95efebcd2
Amber Kim <ambergkim@gmail.com>
2019-02-03 10:04:35 -0800

    proctfile

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(express.static(__dirname + '/build'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/src/build/index.html'));
+  res.sendFile(path.resolve(__dirname, '/build/index.html'));
 });
 
 app.listen(port, (req, res) => {
