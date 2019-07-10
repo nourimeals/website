145d400fe3f4d1a8e56e5897dc2e24debf8eca89
leroyg <leroy.gardner@gmail.com>
2019-07-10 11:15:54 -0500

    Corrected instagram link to Twitter as specified in issue 49 details

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,12 +6,12 @@ import logo from '../../images/nouri-logo.png';
 
 // getting Font Awesome import to component
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faMedium, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faMedium, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const facebookSocial = <FontAwesomeIcon icon={ faFacebook } />
 const linkedinSocial = <FontAwesomeIcon icon={ faLinkedin } />
 const mediumSocial = <FontAwesomeIcon icon={ faMedium } />
-const instagramSocial = <FontAwesomeIcon icon={ faInstagram } />
+const twitterSocial = <FontAwesomeIcon icon={ faTwitter } />
 
 class Footer extends React.Component {
   render() {
@@ -25,8 +25,9 @@ class Footer extends React.Component {
           <div className="social-icons">
             <a href="https://linkedin.com/company/teamnouri">{linkedinSocial}</a>
             <a href="https://medium.com/@teamnouri">{mediumSocial}</a>
-            <a href="https://linkedin.com/company/teamnouri">{facebookSocial}</a>
-            <a href="https://www.facebook.com/teamnouri/">{instagramSocial}</a>
+            <a href="https://www.facebook.com/teamnouri/">{facebookSocial}</a>
+            <a href="https://twitter.com/TeamNouri">{twitterSocial}</a>
+            
           </div>
 
       </div>
