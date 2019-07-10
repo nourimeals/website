abd69362337550323572e188d5c83d63112febb0
leroyg <leroy.gardner@gmail.com>
2019-07-10 11:06:37 -0500

    add social icons (facebook, instagram, linkedin, and medium) to footer, used FontAwesome import

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,17 +2,36 @@ import React from 'react';
 
 import './footer.scss';
 
-import logo from '../../images/nouri-logo.png'
+import logo from '../../images/nouri-logo.png';
 
 // getting Font Awesome import to component
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faMedium, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const facebookSocial = <FontAwesomeIcon icon={ faFacebook } />
+const linkedinSocial = <FontAwesomeIcon icon={ faLinkedin } />
+const mediumSocial = <FontAwesomeIcon icon={ faMedium } />
+const instagramSocial = <FontAwesomeIcon icon={ faInstagram } />
 
 class Footer extends React.Component {
-
   render() {
-    return <footer>
+    return (
+      <footer>
       <img className="footer-logo" src={logo} />
+
+      <div className="social-media">
+        <p>Follow us on Social Media</p>
+    
+          <div className="social-icons">
+            <a href="https://linkedin.com/company/teamnouri">{linkedinSocial}</a>
+            <a href="https://medium.com/@teamnouri">{mediumSocial}</a>
+            <a href="https://linkedin.com/company/teamnouri">{facebookSocial}</a>
+            <a href="https://www.facebook.com/teamnouri/">{instagramSocial}</a>
+          </div>
+
+      </div>
     </footer>
-  }
+    )};
 }
 
 export default Footer;
