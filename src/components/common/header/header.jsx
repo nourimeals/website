8956ef77a6201a073c79042468383c7291ae9a8d
Emily Beissner <kebeissner@gmail.com>
2020-05-27 17:10:26 +0100

    add media query for small phones and tablets

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -7,15 +7,16 @@ import logo from '../../../images/nouri-logo.png';
 export const Header = () => {
   return (
     <header>
-      <div className="header-left">
-        <a href="#welcome"><img src={logo} alt="Nouri Logo" id="logo" /></a>
+      <a href="#welcome"><img src={logo} alt="Nouri Logo" id="logo" /></a>
+      <div className="header-right">
         <ul className="link-row">
           <li><a href="#about">About us</a></li>
-          <li> <a href="#volunteer">Volunteer</a></li>
+          <li><a href="#volunteer">Volunteer</a></li>
           <li><a href="https://docs.google.com/forms/d/18KYoG2Qu9xwRAca-6zuM2wp3i3aa3B3fa4ysegm9g34/viewform?edit_requested=true" target="_blank">Need Help?</a></li>
         </ul>
+        <a href="https://donorbox.org/nouri" target="_blank"><button className="btn-container">donate</button></a>
       </div>
-      <a href="https://donorbox.org/nouri" target="_blank"><button className="btn-container">donate</button></a>
+
     </header>
   );
 }
