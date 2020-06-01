9cc5cbf73a4b207c02cf5221129a4ea0843122f1
Emily Beissner <kebeissner@gmail.com>
2020-06-01 10:54:21 +0100

    fix media queries to reflect mobile first design

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -7,16 +7,17 @@ import logo from '../../../images/nouri-logo.png';
 export const Header = () => {
   return (
     <header>
-      <a href="#welcome"><img src={logo} alt="Nouri Logo" id="logo" /></a>
-      <div className="header-right">
-        <ul className="link-row">
-          <li><a href="#about">About us</a></li>
-          <li><a href="#volunteer">Volunteer</a></li>
-          <li><a href="https://docs.google.com/forms/d/18KYoG2Qu9xwRAca-6zuM2wp3i3aa3B3fa4ysegm9g34/viewform?edit_requested=true" target="_blank">Need Help?</a></li>
-        </ul>
-        <a href="https://donorbox.org/nouri" target="_blank"><button className="btn-container">donate</button></a>
+      <div className="header-parent">
+        <a href="#welcome"><img src={logo} alt="Nouri Logo" id="logo" /></a>
+        <div className="header-right">
+          <ul>
+            <li><a href="#about">About us</a></li>
+            <li><a href="#volunteer">Volunteer</a></li>
+            <li><a href="https://docs.google.com/forms/d/18KYoG2Qu9xwRAca-6zuM2wp3i3aa3B3fa4ysegm9g34/viewform?edit_requested=true" target="_blank">Need Help?</a></li>
+          </ul>
+          <a href="https://donorbox.org/nouri" target="_blank"><button className="btn-container">donate</button></a>
+        </div>
       </div>
-
     </header>
   );
 }
