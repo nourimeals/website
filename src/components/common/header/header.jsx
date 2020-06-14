cfffcf8c682520ce351558f9fe169180f648b19f
Emily Beissner <kebeissner@gmail.com>
2020-06-14 22:23:32 +0100

    removed semantic UI and added font awesome

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { Icon } from 'semantic-ui-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
 
 /*    STYLING    */
 import './header.scss';
 import logo from '../../../images/nouri-logo.png';
 
-const BurgerIcon = () => <Icon name='bars' color='black' size='big' />;
+
 
 export const Header = () => {
 
@@ -23,7 +25,9 @@ export const Header = () => {
             <li><a href="https://docs.google.com/forms/d/18KYoG2Qu9xwRAca-6zuM2wp3i3aa3B3fa4ysegm9g34/viewform?edit_requested=true" target="_blank">Need Help?</a></li>
           </ul>
 
-          <div className="hamburger-icon" onClick={() => showNav(!nav)}><BurgerIcon /></div>
+          <div className="hamburger-icon" onClick={() => showNav(!nav)}>
+            <FontAwesomeIcon icon={faBars} />
+          </div>
 
           <a href="https://donorbox.org/nouri" target="_blank"><button className="btn-container">donate</button></a>
         </div>
