742bc19283c9fc8bdb3135f4ed07e10c6d7f7e43
Emily Beissner <kebeissner@gmail.com>
2020-06-15 19:17:14 +0100

    fix header styling

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -1,36 +1,40 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 /*    STYLING    */
 import './header.scss';
 import logo from '../../../images/nouri-logo.png';
 
-
-
 export const Header = () => {
 
-  const [nav, showNav] = useState(false);
+  const [navClass, toggleNav] = useState("hide");
+
+  const toggleResponsiveNav = () => {
+    if (navClass === "mobile") {
+      toggleNav("hide");
+    } else {
+      toggleNav("mobile");
+    }
+  }
 
   return (
     <header>
       <div className="header-parent">
         <a href="#welcome"><img src={logo} alt="Nouri Logo" id="logo" /></a>
-        <div className="header-right">
 
-          <ul className={`nav-${!nav ? 'hide' : ''}`}>
-            <li><a href="#about">About us</a></li>
-            <li><a href="#volunteer">Volunteer</a></li>
-            <li><a href="https://docs.google.com/forms/d/18KYoG2Qu9xwRAca-6zuM2wp3i3aa3B3fa4ysegm9g34/viewform?edit_requested=true" target="_blank">Need Help?</a></li>
-          </ul>
 
-          <div className="hamburger-icon" onClick={() => showNav(!nav)}>
-            <FontAwesomeIcon icon={faBars} />
-          </div>
-
+        <ul className={navClass}>
+          <li><a href="#about">About us</a></li>
+          <li><a href="#volunteer">Volunteer</a></li>
+          <li><a href="https://docs.google.com/forms/d/18KYoG2Qu9xwRAca-6zuM2wp3i3aa3B3fa4ysegm9g34/viewform?edit_requested=true" target="_blank">Need Help?</a></li>
           <a href="https://donorbox.org/nouri" target="_blank"><button className="btn-container">donate</button></a>
-        </div>
+        </ul>
+
+        <FontAwesomeIcon icon={navClass === "hide" ? faBars : faTimes} size="2x" className="hamburger-icon" onClick={(toggleResponsiveNav)} />
+
+
+
       </div>
     </header>
   );
