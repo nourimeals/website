9026527e7bd0a00e64a72bb13abbd4546a9b7b7e
Randy <randyisbusy@gmail.com>
2020-10-12 13:39:52 -0700

    fixes layout of landing page/click to access MailerLite form/only for mobile small screens adds a div to the hambuger menu

--- src/components/common/header/PopUp.jsx
+++ src/components/common/header/PopUp.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 
 export const PopUp =({popupdisplay, popupFn})=>{ 
     
-    const handleSubmit =(e)=>{
-       popupFn("popupdisplayHIDE");
-    } 
-    const handleClick =(e)=>{
-        e.preventDefault(); 
-         popupFn("popupdisplayHIDE")
-    }
+    
     return(
-    <div className={popupdisplay} onSubmit={(e) => handleClick(e)}> 
-            
-            <div class="ml-form-embed"
+        <div className={popupdisplay}> 
+            <a class="close" href="#" 
+            onClick={(e)=> popupFn("hide")}><b>&times;</b></a>
+            <div className={popupdisplay} className="ml-form-embed"
                 data-account="1308892:t0g9u5n8v8"
                 data-form="1094822:b7x6u1">
-            </div>
+            </div> 
+            
     </div>)
 
 } 
