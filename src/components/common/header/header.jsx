4ea3a48a8845f22336f1e7c537d847113280d5b9
Amber Kim <14047327+ambergkim@users.noreply.github.com>
2020-10-12 17:09:07 -0700

    Cleanup (#117)
    * Cleanup
    
    What changed:
    - removing the script in index.html
    - keeping ul to only hold li items
    - moved popup outside of the ul
    
    * fixes layout of landing page/click to access MailerLite form/only for mobile small screens adds a div to the hambuger menu
    
    Co-authored-by: Randy <randyisbusy@gmail.com>

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -10,7 +10,7 @@ import logo from '../../../images/nouri-logo.png';
 export const Header = () => {
   
   const [navClass, toggleNav] = useState("hide");
-  const [POPUPDISPLAY, popUp] = useState('popupdisplayHIDE'); 
+  const [POPUPDISPLAY, popUp] = useState('hide'); 
   //const [submitEmail, name, email] = useState('name', 'email');  
   const toggleResponsiveNav = () => {
     if (navClass === "mobile") {
@@ -19,26 +19,13 @@ export const Header = () => {
       toggleNav("mobile");
     }
   }   
-
-  //fn handleSubmitEmail is not used, could be used later
-  // const handleSubmitEmail = (e)=>{
-  //    //this fn is never run and can be used in the future 
-  //    submitEmail(e.target.name.value, e.target.email.value) 
-  //    console(e, name, email )
-
-  //  } 
   const handlePopUp =(e)=>{ 
     //toggle effect for the popUp component to get emails from nouri 
     e.preventDefault();  
-    if (POPUPDISPLAY === "popupdisplaySHOW"){
-      popUp("popupdisplayHIDE") 
-    } else {
-      popUp("popupdisplaySHOW")
-    }
-    
+    if (POPUPDISPLAY === "hide") { popUp("overlay") } else { popUp("hide") }
   }
 
-  return (
+  return (<>
     <header>
       <div className="header-parent">
         <a href="#welcome"><img src={logo} alt="Nouri Logo" id="logo" /></a>
@@ -47,18 +34,24 @@ export const Header = () => {
           <li><a href="#volunteer">Volunteer</a></li>
           <li><a href="https://forms.gle/pK6fb7YfQMWqNnRC7" target="_blank">Need Help?</a></li>
           <li><a href="https://github.com/nourimeals/documents/tree/master/Webinars" target="_blank">Resources</a></li>
-          <button onClick={(e)=> handlePopUp(e)} className="btn-container">Connect With Email</button>
+          {/* adds div to only smaller || "mobile" devices that allows users to access MailerLite form*/}
+          <li><div className={navClass}><a onClick={(e)=> handlePopUp(e)}>Connect With Email</a></div>
+            <button onClick={(e) => handlePopUp(e)} className="btn-container">Connect With Email</button>
+          </li>
           { /* passing the variable POPUPDISPLAY and the setState function popUP via props */}
-          <PopUp popupdisplay={POPUPDISPLAY} popupFn={popUp} /*submitEmail={handleSubmitEmail}*/ />
-          <a href="https://donorbox.org/nouri" target="_blank"><button className="btn-container">donate</button></a>
+          <li>
+            <a href="https://donorbox.org/nouri" target="_blank"><button className="btn-container">donate</button></a>
+          </li>
         </ul>
 
         <FontAwesomeIcon icon={navClass === "hide" ? faBars : faTimes} size="2x" className="hamburger-icon" onClick={(toggleResponsiveNav)} />
 
-
-
       </div>
-    </header>
+
+     
+    </header> 
+    <PopUp popupdisplay={POPUPDISPLAY} popupFn={popUp} />
+    </>
   );
 }
 
